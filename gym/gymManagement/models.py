c2450edb16de698db05d49f60543c78747b5a307
iphadan <ephadaniel177@gmail.com>
2024-04-30 11:19:20 +0300

    dynamic dashboard

--- gym/gymManagement/models.py
+++ gym/gymManagement/models.py
@@ -1,3 +1,35 @@
+import datetime
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+# class gymAdmin(models.Model):
+#     user=models.ForeignKey(User,on_delete=models.CASCADE)
+
+class Plan(models.Model):
+    title=models.CharField(max_length=50)
+    price=models.IntegerField(default=0)
+    createdAt=models.DateTimeField(default=datetime.datetime.today())
+
+
+    def __str__(self) -> str:
+        return self.title + " " + str(self.price)
+
+class GymMember(models.Model):
+    firstName=models.CharField(max_length=50)
+    lastName=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+    phone=models.CharField(max_length=14,default='0900000000')
+    photo=models.ImageField(upload_to='upload/profile',blank=True,null=True)
+    plan=models.ForeignKey(Plan,on_delete=models.SET_NULL,null=True,blank=True)
+    joinedAt=models.DateField(default=datetime.date.today())
+    paidAt=models.DateTimeField(default=datetime.datetime.today())
+    active=models.BooleanField(default=True)
+
+
+    def __str__(self) -> str:
+        return self.firstName + " " + self.lastName
+
+
+
+
+    
--- gym/gymManagement/views.py
+++ gym/gymManagement/views.py
@@ -1,8 +1,33 @@
+import datetime
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    newGymMembers=models.GymMember.objects.filter(joinedAt=datetime.date.today())
+    gymMembers=models.GymMember.objects.filter(joinedAt__year=datetime.date.today().year)
+    thisMonthGymMembers=models.GymMember.objects.filter(joinedAt__month=datetime.date.today().month)
+    print(thisMonthGymMembers)
+    thisMonthRevenue=0
+    for thisMonthGymMember in thisMonthGymMembers:
+        if thisMonthGymMember.plan.price:
+            thisMonthRevenue+=thisMonthGymMember.plan.price
+
+    
+   
+    context={
+        'newGymMembers':newGymMembers.__len__(),
+        'gymMembers':gymMembers.__len__(),
+        'thisMonthGymMembers':thisMonthGymMembers,
+        'thisMonthRevenue':thisMonthRevenue
+
+    }
+
+    {
+
+    }
+
+    return render(request,'index.html',context)
 
 def manageMembers(request):
     return render(request,'manageMembers.html')
