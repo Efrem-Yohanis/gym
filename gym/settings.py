58b834bce490e19b33ee81afdd2a84fe71ec5fa2
Efrem-Yohanis-Anbese <efremyohanis116@gmail.com>
2024-05-03 22:09:41 +0300

    static file

--- gym/settings.py
+++ gym/settings.py
@@ -130,7 +130,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '/static/'),
     # Other directories if needed
 ]
 
